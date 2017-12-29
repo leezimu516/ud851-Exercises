18862dec2f6e584353d9d7a3bc74df1cdd3a7928
kai li <adamlee516@gmail.com>
2017-12-29 11:43:25 -0500

    Build uri

--- Lesson09-ToDo-List/T09.02-Exercise-AddURIsToContract/app/src/main/java/com/example/android/todolist/data/TaskContract.java
+++ Lesson09-ToDo-List/T09.02-Exercise-AddURIsToContract/app/src/main/java/com/example/android/todolist/data/TaskContract.java
@@ -16,6 +16,7 @@
 
 package com.example.android.todolist.data;
 
+import android.net.Uri;
 import android.provider.BaseColumns;
 
 
@@ -30,10 +31,19 @@ public class TaskContract {
         4) Content URI for data in the TaskEntry class
       */
 
+    // The authority, which is how your code knows which Content Provider to access
+    public static final String AUTHORITY = "com.example.android.todolist";
+    // The base content URI = "content://" + <authority>
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + AUTHORITY);
+    // Define the possible paths for accessing data in this contract
+    // This is the path for the "tasks" directory
+    public static final String PATH_TASK = "task";
 
     /* TaskEntry is an inner class that defines the contents of the task table */
     public static final class TaskEntry implements BaseColumns {
 
+        // TaskEntry content URI = base content URI + path
+        public static final Uri CONTENT_URI = BASE_CONTENT_URI.buildUpon().appendPath(PATH_TASK).build();
 
         // Task table and column names
         public static final String TABLE_NAME = "tasks";
